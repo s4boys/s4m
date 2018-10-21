98c49a866ec56251e0be1fbc94138b30cbfca8f3
Qunnlin <71scfe1bif@hft-stuttgart.de>
2018-10-21 19:57:17 +0200

    .

--- THI/Felix/Selfprint/src/selfprint/SelfPrint.java
+++ THI/Felix/Selfprint/src/selfprint/SelfPrint.java
@@ -1,54 +1,48 @@
 package selfprint;
 public class SelfPrint {
     public static void main(String[] args) {
-    	System.out.println(getText());
-    }	
+        System.out.println(getText());
+    }
     // Ab hier einfuegen erlaubt
     private static String programText [] = {
 "package selfprint;",
 "public class SelfPrint {",
-"   public static void main(String[] args) {",
-"	System.out.println(getText());",
-"   }",
-"   // Ab hier einfuegen erlaubt",
-"   private static String programText [] = {",
-"   };",
-"   private static String getText() {",
-"       char quote = 34;",
-"       char newline = 10;",		
-"       String sourceCode = new String();",
-"       for (int i = 0; i < 7; i++) {",
-"           sourceCode = sourceCode + programtext[i] + newline;",			
-"       }",
-"       for (int i = 0; i < programText.length; i++) {",
-"           sourceCode = sourceCode + qoute + programText[i] + qoute + ',' + newline;",		
-"       }",
-"       for(int i = 7; i < programText.length; i++) {",
-"           sourceCode = sourceCode + programText[i] + newline;",
-"       }",
-"       for(int i = programText.length-1; i < programText.length; i++) {",
-"           sourceCode = sourceCode + programText[i];",
-"       }",
-"       return sourceCode;",
-"   }",
-"   // Ab hier einfuegen verboten",
+"    public static void main(String[] args) {",
+"        System.out.println(getText());",
+"    }",
+"    // Ab hier einfuegen erlaubt",
+"    private static String programText [] = {",
+"    };",
+"    private static String getText() {",
+"        char quote = 34;",
+"        char newline = 10;",
+"        String sourceCode = new String();",
+"        for (int i = 0; i < 7; i++) {",
+"            sourceCode = sourceCode + programtext[i] + newline;",
+"        }",
+"        for (int i = 0; i < programText.length; i++) {",
+"            sourceCode = sourceCode + qoute + programText[i] + qoute + ',' + newline;",
+"        }",
+"        for(int i = 7; i < programText.length; i++) {",
+"            sourceCode = sourceCode + programText[i] + newline;",
+"        }",
+"        return sourceCode;",
+"    }",
+"    // Ab hier einfuegen verboten",
 "}",
-    };	
+    };
     private static String getText() {
-	char quote = 34;
-	char newline = 10;
-	String sourceCode = new String();
-	for (int i = 0; i < 7; i++) {
+        char quote = 34;
+        char newline = 10;
+        String sourceCode = new String();
+        for (int i = 0; i < 7; i++) {
             sourceCode = sourceCode + programText[i] + newline;
-	}
-	for (int i = 0; i < programText.length; i++) {
-            sourceCode = sourceCode + quote + programText[i] + quote + ',' + newline;                     
-	}
-	for(int i = 7; i < programText.length-1; i++) {
+        }
+        for (int i = 0; i < programText.length; i++) {
+            sourceCode = sourceCode + quote + programText[i] + quote + ',' + newline;
+        }
+        for(int i = 7; i < programText.length; i++) {
             sourceCode = sourceCode + programText[i]+ newline;
-	}
-        for(int i = programText.length-1; i < programText.length; i++) {
-            sourceCode = sourceCode + programText[i]; 
         }
         return sourceCode;
     }
